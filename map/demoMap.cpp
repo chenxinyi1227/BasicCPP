d53222ac655aa11060295cb28198e2c816dc6d58
cxy <2830206832@qq.com>
2024-01-17 11:11:08 +0800

    map

--- map/demoMap.cpp
+++ map/demoMap.cpp
@@ -1,9 +1,30 @@
+#include <iostream>
+using namespace std;
 #include <stdio.h>
 #include <map>
 
+typedef struct stuInfo
+{
+    int age;
+    int height;
+    int weight;
+    int sex;
+}stuInfo;
+
 int main()
 {
+    map<int,int> mymap;
+    /* 7这个位置来决定 */
+    mymap[7] = 666;
 
-    
+    printf("map:%d\n", mymap[7]);
+
+
+    stuInfo info;
+    info.age = 20;
+    info.sex = 'm';
+    map<string, stuInfo> mymap2;
+    mymap2["xiaozhou"] = info;
+    printf("xiaozhou age :%d sex:%c\n", mymap2["xiaohou"].age, mymap2["xiaozhou"].sex);
     return 0;
 }
