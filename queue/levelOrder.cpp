2c3a6f24d980494c3b70cf6407172ebe4ca86bee
cxy <2830206832@qq.com>
2024-01-18 21:01:01 +0800

    调整空格

--- queue/levelOrder.cpp
+++ queue/levelOrder.cpp
@@ -30,39 +30,38 @@ vector<vector<int>> levelOrder(TreeNode* root)
     }
     /* 以下程序不是空树 */
 
-        queue<TreeNode *> myqueue;
-        /* 根结点入队 */
-        myqueue.push(root);
-        TreeNode *frontNode = NULL;//队列头号元素
-        
-        /* 每一层结点的数量 */
-        int levelSize = 0;
-        while(!myqueue.empty())
-        {    
-            /* 每一层的结点的数组 */
-            vector<int> levelVector;
-
-            /* 每一层结点的数量 */
-            levelSize  = myqueue.size();
-
-            for(int idx = 0; idx < levelSize; idx++)
-            {
-                frontNode = myqueue.front();
-                levelVector.push_back(frontNode->val);
-                myqueue.pop();
-                
-                if(frontNode->left)
-                {
-                    myqueue.push(frontNode->left);
-                }
-                if(frontNode->right)
-                {
-                    myqueue.push(frontNode->right);
-                }
-            }
-            res.push_back(levelVector);
-        }
-        return res;
+    queue<TreeNode *> myqueue;
+    /* 根结点入队 */
+    myqueue.push(root);
+    TreeNode *frontNode = NULL;//队列头号元素
     
+    /* 每一层结点的数量 */
+    int levelSize = 0;
+    while(!myqueue.empty())
+    {    
+        /* 每一层的结点的数组 */
+        vector<int> levelVector;
+
+        /* 每一层结点的数量 */
+        levelSize  = myqueue.size();
+
+        for(int idx = 0; idx < levelSize; idx++)
+        {
+            frontNode = myqueue.front();
+            levelVector.push_back(frontNode->val);
+            myqueue.pop();
+            
+            if(frontNode->left)
+            {
+                myqueue.push(frontNode->left);
+            }
+            if(frontNode->right)
+            {
+                myqueue.push(frontNode->right);
+            }
+        }
+        res.push_back(levelVector);
+    }
+    return res;
 }
    
